b31b4e01afc01cf17e99dc205255026bfa8984dd
dnj <2643272363@qq.com>
2021-01-28 19:22:40 +0800

    点击高亮和与歌曲页面的交互

--- miniprogram/components/musiclist/musiclist.js
+++ miniprogram/components/musiclist/musiclist.js
@@ -1,26 +1,25 @@
 // components/musiclist/musiclist.js
 const app = getApp()
 Component({
-  /**
-   * 组件的属性列表
-   */
+
   properties: {
     musiclist:Array
   },
 
-  /**
-   * 组件的初始数据
-   */
   data: {
-
+    playingId:-1
   },
 
-  /**
-   * 组件的方法列表
-   */
   methods: {
-    onSelect(){
-      console.log('ok')
+    onSelect(event){
+      const ds = event.currentTarget.dataset
+      console.log(ds)
+      this.setData({
+        playingId:event.currentTarget.dataset.musicid
+      })
+      wx.navigateTo({
+        url: `../../pages/player/player?musicId=${ds.musicid}&index=${ds.index}`,
+      })
     }
   }
 })
